72363e313ff6e5455911619c4b23eb4b4dd7e983
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-28 14:00:04 -0700

    save progress

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
-//Business Logic
-var userInput = parseInt($("input#userInput").val());
-var total = 0;
-
 //User Interface Logic
 $(document).ready(function() {
-  $("form").submit(function(event) {
+  $("form#pingPong").submit(function(event) {
     event.preventDefault();
-
-    //for loop
-    for (var i = 1; i <= total; i++) {
-      //conditional statements
-      if ((i % 3 === 0 && i % 5 === 0)) { //if index is divisible by 3 OR 5
-        ("<b>PING-PONG</b>");
-      }
-      else if (i % 3 === 0) { //if index is divisible by 3
-        ("**PING**");
-      }
-      else if (i % 5 === 0) { //if index is divisible by 5
-        ("**PONG**");
-      }
-      else if (i % 18 === 0) { //if index is divisible by 18
-        ("**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**");
-      }
-
-    $("div#result").show();
-      $("ul#result-list").append("<li>" + total + "</li>");
-    }
   });
 });
+
+function playPong(userInput) {
+  var array = [];
+    //for loop
+    for (i = 1; i <= userInput; i++) {
+      //conditional statements
+      if ((i % 3 === 0 && i % 5 === 0)) { //if index is divisible by 3 OR 5
+        array.push("**PING-PONG**"); //add message to the end
+      }
+      else if (i % 3 === 0) { //if index is divisible by 3
+        array.push("**PING**");
+      }
+      else if (i % 5 === 0) { //if index is divisible by 5
+        array.push("**PONG**");
+      }
+      else if (i % 18 === 0) { //if index is divisible by 18
+        array.push("**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**");
+      }
+      else {
+        array.push(i);
+      }
+    }
+  return array;
+}
