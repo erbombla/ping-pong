abdc16eb4c8af17ff177348eda4f3416965a1a1b
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-28 13:12:30 -0700

    revised logic, site doesn't crash anymore

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
 //Business Logic
-
-//Setup empty array?
-
-function
-  //for loop
-  for (i = 1; i <= input; i++) {
-    //conditional statements
-    if ((i % 3 === 0 && num % 5 === 0)) { //if index is divisible by 3 OR 5
-      counter.push("<b>PING-PONG</b>");
-    }
-    else if (i % 3 === 0) { //if index is divisible by 3
-      counter.push("**PING**");
-    }
-    else if (i % 5 === 0) { //if index is divisible by 5
-      counter.push("**PONG**");
-    }
-    else if (i % 18 === 0) { //if index is divisible by 18
-      counter.push("**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**");
-    }
-  }
-};
+var userInput = parseInt($("input#userInput").val());
+var total = 0;
 
 //User Interface Logic
 $(document).ready(function() {
-  $("form#userInput").submit(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
+
+    //for loop
+    for (var i = 1; i <= total; i++) {
+      //conditional statements
+      if ((i % 3 === 0 && i % 5 === 0)) { //if index is divisible by 3 OR 5
+        ("<b>PING-PONG</b>");
+      }
+      else if (i % 3 === 0) { //if index is divisible by 3
+        ("**PING**");
+      }
+      else if (i % 5 === 0) { //if index is divisible by 5
+        ("**PONG**");
+      }
+      else if (i % 18 === 0) { //if index is divisible by 18
+        ("**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**");
+      }
+
+    $("div#result").show();
+      $("ul#result-list").append("<li>" + total + "</li>");
+    }
   });
 });
