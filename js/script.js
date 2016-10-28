2efcd3d76055c6fe7350ac9b35c56d87484eba7c
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-28 15:15:46 -0700

    revise logic, displays a number but not intended outcome

--- js/script.js
+++ js/script.js
@@ -2,29 +2,32 @@
 $(document).ready(function() {
   $("form#pingPong").submit(function(event) {
     event.preventDefault();
+    $("div#result").show();
+    $("ul#result-list").append("<li>" + total + "</li>");
   });
 });
 
-function playPong(userInput) {
-  var array = [];
+//Buisness Interface Logic
+  var userInput = parseInt($("input#userInput").val());
+  var range = [];
+  var total = 0;
     //for loop
-    for (i = 1; i <= userInput; i++) {
+    for (var i = 1; i <= range; index++) {
+      total += userInput;
       //conditional statements
       if ((i % 3 === 0 && i % 5 === 0)) { //if index is divisible by 3 OR 5
-        array.push("**PING-PONG**"); //add message to the end
+        range.push("**PING-PONG**"); //add message to the end
       }
       else if (i % 3 === 0) { //if index is divisible by 3
-        array.push("**PING**");
+        range.push("**PING**");
       }
       else if (i % 5 === 0) { //if index is divisible by 5
-        array.push("**PONG**");
+        range.push("**PONG**");
       }
       else if (i % 18 === 0) { //if index is divisible by 18
-        array.push("**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**");
+        range.push("**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**");
       }
       else {
-        array.push(i);
+        range.push(i); //final result
       }
-    }
-  return array;
-}
+  }
