7768bb2d24de2e9bf68c5117d7d8847c48c5b12e
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-28 15:40:23 -0700

    add and wrote README.md

---
+++ README.md
@@ -0,0 +1,45 @@
+# _PING-PONG_
+
+#### Displays integers divisible by 3, 5 and 15 using a for loop, Oct.28,2016
+
+#### **By Eric Bomblatus**
+
+## Description
+
+_Specifications_
+* Numbers divisible by 3 are replaced with "ping"
+* Numbers divisible by 5 are replaced with "pong"
+* Numbers divisible by 15 (3 and 5) are replaced with "ping-pong"
+
+_Inputs_-->_Outputs_
+* 3,6,9 --> ping
+* 5, 10, 20 --> pong
+* 15, 30, 45 --> ping-pong
+
+## Setup/Installation Requirements
+* Clone this repository
+* Open the folder to view contents in a text-editor, like Atom
+* Commit and Push edits as needed
+
+## Known Bugs
+
+No known bugs
+
+## Support and contact details
+
+If you run into any issues or have questions, ideas or concerns you may contact me at ebomblatus@gmail.com. Thank you.
+
+## Technologies Used
+
+This repository is written in:
+
+* HTML
+* CSS
+* JavaScript
+* jQuery
+
+## Link
+
+### License
+
+Copyright (c) 2016 **Eric Bomblatus**
--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-//User Interface Logic
-$(document).ready(function() {
-  $("form#pingPong").submit(function(event) {
-    event.preventDefault();
-    $("div#result").show();
-    $("ul#result-list").append("<li>" + total + "</li>");
-  });
-});
-
 //Buisness Interface Logic
   var userInput = parseInt($("input#userInput").val());
-  var range = [];
+  var arr = [];
   var total = 0;
     //for loop
-    for (var i = 1; i <= range; index++) {
+    for (var i = 1; i <= total; i++) {
       total += userInput;
       //conditional statements
       if ((i % 3 === 0 && i % 5 === 0)) { //if index is divisible by 3 OR 5
-        range.push("**PING-PONG**"); //add message to the end
+        arr.push("**PING-PONG**"); //add message to the end
       }
       else if (i % 3 === 0) { //if index is divisible by 3
-        range.push("**PING**");
+        arr.push("**PING**");
       }
       else if (i % 5 === 0) { //if index is divisible by 5
-        range.push("**PONG**");
+        arr.push("**PONG**");
       }
       else if (i % 18 === 0) { //if index is divisible by 18
-        range.push("**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**");
+        arr.push("**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**");
       }
       else {
-        range.push(i); //final result
+        arr.push(i); //final result
       }
   }
+
+  //User Interface Logic
+  $(document).ready(function() {
+    $("form#pingPong").submit(function(event) {
+      event.preventDefault();
+      $("div#result").show();
+      $("ul#result-list").append("<li>" + total + "</li>");
+    });
+  });
