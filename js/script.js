c643673a39de80e36f3b579129dd932d57bc6a1f
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-28 10:37:38 -0700

    separated buisness from user interface logic

--- js/script.js
+++ js/script.js
@@ -1,31 +1,29 @@
 //Business Logic
-variables
-
+var myNum = function(num) {
   //for loop
-  for (i = 1; 1 <= num; i++) {
-    listItems += "<li>";
+  for (num = 1; num <= input; num++) {
     //conditional statements
-    if ((i % 3 === 0) && (i % 5 === 0)){ //if index is divisible by 3 OR 5
-      listItems += "<b>PING-PONG</b>"
+    if ((num % 3 === 0 && num % 5 === 0)) { //if num is divisible by 3 OR 5
+      "<b>PING-PONG</b>"
     }
-    else if (i % 3 === 0) { //if index is divisible by 3
-      listItems += "<b>PING</b>"
+    else if (num % 3 === 0) { //if num is divisible by 3
+      "<b>PING</b>"
     }
-    else if (i % 5 === 0) { //if index is divisible by 5
-      listItems += "<b>PONG</b>"
+    else if (i % 5 === 0) { //if num is divisible by 5
+      "<b>PONG</b>"
     }
-    else if (i % 18 === 0) { //if index is divisible by 18 
-      listItems += "<b>**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**</b>"
+    else if (i % 18 === 0) { //if num is divisible by 18
+      "<b>**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**</b>"
     }
-    else {
-      listItems += i;
-    }
-    listItems += "</li>";
-  }
+  };
 
 //User Interface Logic
 $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
+    var myNum = parseInt($("input#myNum").val());
+    var total = 0;
+    $("div#results").show();
+    $("ul#result-list").append("<li>" + total + "</li>");
   });
 });
