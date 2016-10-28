dce7b1c0a9d3c8450c9bc89030e3e48c544589a6
Eric Bomblatus <ebomblatus@gmail.com>
2016-10-28 09:30:58 -0700

    add logic for scripts.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
-$(document).ready(function() {
-  $("form#userInput").submit(function(event) {
-    var myNum = parseInt($("input#myNum").val());
-    var total = 0
+//Business Logic
+variables
 
-    if (isNaN(myNum) === true || isNaN(stopNum) === true) {
-      alert("Please enter a number");
+  //for loop
+  for (i = 1; 1 <= num; i++) {
+    listItems += "<li>";
+    //conditional statements
+    if ((i % 3 === 0) && (i % 5 === 0)){ //if index is divisible by 3 OR 5
+      listItems += "<b>PING-PONG</b>"
+    }
+    else if (i % 3 === 0) { //if index is divisible by 3
+      listItems += "<b>PING</b>"
+    }
+    else if (i % 5 === 0) { //if index is divisible by 5
+      listItems += "<b>PONG</b>"
+    }
+    else if (i % 18 === 0) { //if index is divisible by 18 
+      listItems += "<b>**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**PEPPERCORN PING-PONG**</b>"
     }
     else {
-
-      for (var startNum = 0; total < stopNum; startNum +- 0) {
-        total += myNum;
-
-        $("div#result").show();
-        $("ul#result-list").append("<li>" + total + "</li>");
-        // console.log(total);
-      }
+      listItems += i;
     }
+    listItems += "</li>";
+  }
+
+//User Interface Logic
+$(document).ready(function() {
+  $("form#userInput").submit(function(event) {
     event.preventDefault();
   });
 });
