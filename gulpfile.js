73ed30abde6bed428efed8d624f5bfa8c602c111
Eric Bomblatus and Rafael  Perez <eric+rafael@>
2017-01-02 09:27:40 -0800

    install npm packages and debug js

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var jshint = require('gulp-jshint');
+
+
+gulp.task('jsBrowserify', function() {
+  return browserify({ entries: ['./js/pingpong-interface.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
--- js/pingpong-interface.js
+++ js/pingpong-interface.js
@@ -1,13 +1,14 @@
 var Calculator = require('./../js/pingpong.js').calculatorModule;
 
 $(document).ready(function() {
-  $("#ping-pong-form").submit(function(event) {
+  $('#ping-pong-form').submit(function(event) {
     event.preventDefault();
     var goal = $('#goal').val();
     var simpleCalculator = new Calculator("hot pink");
     var output = simpleCalculator.pingPong(goal);
+    $('#solution').empty();
     output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");                                                                                                                      (number);
+      $('#solution').append("<li>" + element + "</li>");
     });
   });
 });
